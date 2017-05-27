2e6a2c73cee961ca0669c1ab639a4b72e615e980
Shane Becker <veganstraightedge@gmail.com>
2017-05-26 22:10:36 -0700

    Add bootstrap styles to views

--- result.php
+++ result.php
@@ -1,32 +1,85 @@
+<!DOCTYPE html>
 <html>
 <head>
-	<title>Microformats Parser</title>
-	<link rel="stylesheet" href="styles.css">
-</head>
-<body>
-	<div id="mf2">
-    <h3>Microformats Parser</h3>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-      <div class="parser input">
-  			<form action="/mf2/" method="post">
-    			<div class="title">HTML:</div>
-  				<textarea name="html" style="width: 100%; padding: 2px; height: 200px" class="text"><?= htmlspecialchars($html) ?></textarea><br>
-    			<div class="title">Base URL:</div>
-  				<input type="text" name="url" style="width: 100%; padding: 2px;" class="text" value="<?= $url ?>" /><br>
-          <div class="title">JSON:</div>
-  				<textarea style="width: 100%; padding: 2px; height: 300px" class="text"><?= htmlspecialchars($json) ?></textarea><br>
-					<input type="checkbox" id="save" name="save" <?= $save_html ? 'checked="checked"' : '' ?> value="1"> Save HTML<br>
-          <input type="checkbox" id="show_html" name="show_html" value="1" <?= $show_html ? 'checked="checked"' : '' ?>> Render HTML in page<br>
-  				<input type="submit" value="Parse" />
-  			</form>
+  <title>PHP Microformats Parser</title>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+  <main id="mf2" class="container">
+
+    <h1 class="mt-5 mb-3">
+      Microformats Parser (PHP)
+      <!-- TODO print version of php-mf2 here -->
+    </h1>
+
+    <form method="post" action="/mf2" class="mb-5">
+      <div class="form-group">
+        <label for="html">HTML</label>
+        <textarea id="html" name="html" rows="6" class="form-control form-control-lg"><?= htmlspecialchars($html) ?></textarea>
       </div>
 
-      <?php if($show_html): ?>
-        <hr>
-        <?= $html ?>
-        <hr>
-      <?php endif; ?>
+      <div class="form-group">
+        <label for="base-url">Base URL</label>
+        <input id="base-url" name="url" type="url" class="form-control form-control-lg" value="<?= $url ?>" />
+      </div>
 
-	</div>
+      <div class="form-check">
+        <label class="form-check-label" for="save">
+          <input id="save" name="save" class="form-check-input" type="checkbox" value="1" <?= $save_html ? 'checked="checked"' : '' ?>>
+          Save HTML?
+        </label>
+      </div>
+
+      <div class="form-check">
+        <label class="form-check-label" for="show_html">
+          <input id="show_html" name="show_html" class="form-check-input" type="checkbox" value="1" <?= $show_html ? 'checked="checked"' : '' ?>>
+          Render HTML in page?
+        </label>
+      </div>
+
+      <div class="form-group">
+        <label for="json">JSON</label>
+        <textarea id="json" name="json" rows="6" class="form-control form-control-lg" disabled="disabled"><?= htmlspecialchars($json) ?></textarea>
+      </div>
+
+      <button type="submit" class="btn btn-lg btn-success">Parse</button>
+    </form>
+
+    <?php if($show_html): ?>
+
+      <div class="card mb-5">
+        <div class="card-header">
+          Rendered HTML
+        </div>
+
+        <div class="card-block">
+          <?= $html ?>
+        </div>
+      </div>
+    <?php endif; ?>
+
+    <footer class="my-5">
+      <ul>
+        <li><a href="https://microformats.io">About Microformats</a></li>
+        <li><a href="https://github.com/aaronpk/mf2-test">Source code for this site</a></li>
+        <li><a href="https://github.com/indieweb/php-mf2">Source code for the Microformats PHP Parser</a></li>
+
+        <li>
+          Other Microformats Parser websites:
+          <a href="https://go.microformats.io">Go</a>,
+          <a href="https://node.microformats.io">Node</a>,
+          <a href="https://python.microformats.io">Python</a>, and
+          <a href="https://ruby.microformats.io">Ruby</a>.
+        </li>
+      </ul>
+    </footer>
+
+  </main>
 </body>
 </html>
